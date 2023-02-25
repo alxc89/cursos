3dc16e9bbefaa45e46b10a8717b7c794615dfd03
alxc89 <alexcaardoso@gmail.com>
2023-02-24 23:40:18 -0300

    SqlParameter

--- balta.io/Dapper/DataAccess/Program.cs
+++ balta.io/Dapper/DataAccess/Program.cs
@@ -68,20 +68,31 @@ namespace DataAccess
             var insertSql = @"INSERT INTO 
                     [Category] 
                 VALUES(
-                    Id, 
-                    title, 
-                    url, 
-                    summary, 
-                    order, 
-                    description, 
-                    featured)";
+                    @Id, 
+                    @title, 
+                    @url, 
+                    @summary, 
+                    @order, 
+                    @description, 
+                    @featured)";
 
 
             using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conectado!");
 
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Description,
+                    category.Order,
+                    category.Summary,
+                    category.Featured
+                });
 
+                Console.WriteLine($"{rows} - Linhas inseridas");
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
                 foreach (var item in categories)
                 {
