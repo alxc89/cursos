0508bdea39521d6fc767d4835d48731f42a24ef6
alxc89 <alexcaardoso@gmail.com>
2023-02-27 23:04:28 -0300

    OneToMany

---
+++ balta.io/Dapper/DataAccess/Models/Career.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Career
+    {
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public IList<CareerItem> Items { get; set; }
+    }
+}
