185be911e4d6b7012a27aa6a6d3ac0345dc20e80
alxc89 <alexcaardoso@gmail.com>
2023-03-01 22:03:28 -0300

    CriandoProjetoBlog

---
+++ balta.io/Dapper/Blog/Program.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;Integrated Security=true;Trust Server Certificate=true";
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                    System.Console.WriteLine(user.Name);
+            }
+        }
+    }
+}
