f08632a57b6382f260709c5f26c7db3862bf3068
alxc89 <alexcaardoso@gmail.com>
2023-05-28 23:36:22 -0300

    Stored Procedures

---
+++ balta.io/Fundamentos do SQL Server/Stored Procedures.sql
@@ -0,0 +1,9 @@
+--Sintaxer para criar
+CREATE OR ALTER PROCEDURE [spListCourse] AS 
+    SELECT TOP 100
+        *
+    FROM 
+        [Curso]
+
+
+DROP PROCEDURE [spListCourse] -- Sintaxe para dropar
