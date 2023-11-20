4f41b8711cc204f2d6197a3ad6a15842e098d5e8
Alex Cardoso da Costa <55769293+alxc89@users.noreply.github.com>
2023-11-20 00:10:57 -0300

    Paralelismo
    Pequeno estudo sobre Paralelismo.

---
+++ youtube/c#/Paralelismo/Paralelismo/Exemplo1/Exemplo1.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Paralelismo.Exemplo1;
+
+public static class Exemplo1
+{
+    public static void Iniciar()
+    {
+        var stopWatch = new Stopwatch();
+        var acao1 = new Action(Processo1);
+        var acao2 = new Action(Processo2);
+        var acao3 = new Action(Processo3);
+        stopWatch.Start();
+        Parallel.Invoke(acao1, acao2, acao3);
+        stopWatch.Stop();
+        Console.WriteLine($"O tempo de processamento total é de {stopWatch.ElapsedMilliseconds} ms");
+    }
+
+    static void Processo1()
+    {
+        Console.WriteLine($"Processo 1 finalizado Thread: {Environment.CurrentManagedThreadId}");
+        Thread.Sleep(1000);
+    }
+
+    static void Processo2()
+    {
+        Console.WriteLine($"Processo 2 finalizado Thread: {Environment.CurrentManagedThreadId}");
+        Thread.Sleep(1000);
+    }
+
+    static void Processo3()
+    {
+        Console.WriteLine($"Processo 3 finalizado Thread: {Environment.CurrentManagedThreadId}");
+        Thread.Sleep(1000);
+    }
+}
