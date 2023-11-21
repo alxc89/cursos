1daf0d398d82c56b0ddf5aaeac57fe361f4afef6
Alex Cardoso da Costa <55769293+alxc89@users.noreply.github.com>
2023-11-20 23:18:31 -0300

    TaskFactory

---
+++ youtube/c#/TaskFactory/TaskFactory/Program.cs
@@ -0,0 +1,7 @@
+﻿using TaskFactory.CShp_Task;
+using TaskFactory.TaskWaitAll;
+using TaskFactory.TaskWaitAny;
+
+//CShp_Task.FazerAlgo();
+//TaskWaitAll.Main();
+TaskWaitAny.Main();
---
+++ youtube/c#/TaskFactory/TaskFactory/TaskWaitAny/TaskWaitAny.cs
@@ -0,0 +1,31 @@
+﻿namespace TaskFactory.TaskWaitAny;
+
+public static class TaskWaitAny
+{
+    public static void Main()
+    {
+        var TaskA = Task.Factory.StartNew(MetodoA);
+        var TaskB = Task.Factory.StartNew(MetodoB);
+
+        Console.WriteLine("TaskA id = {0}", TaskA.Id);
+        Console.WriteLine("TaskB id = {0}", TaskB.Id);
+
+        var tarefas = new Task[] { TaskA, TaskB };
+        int qualTask = Task.WaitAny(tarefas);
+
+        Console.WriteLine($"A Task de id {tarefas[qualTask].Id} acabou primeiro");
+        Console.ReadLine();
+    }
+
+    public static void MetodoA()
+    {
+        Console.WriteLine("MetodoA");
+        Thread.SpinWait(100000000);
+    }
+
+    public static void MetodoB()
+    {
+        Console.WriteLine("MetodoB");
+        Thread.SpinWait(100000000 / 2);
+    }
+}
