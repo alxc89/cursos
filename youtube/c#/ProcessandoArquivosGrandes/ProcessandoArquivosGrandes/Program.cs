a55c1a310df7bc395ec48c3a4242720ab72de1be
Alex Cardoso da Costa <55769293+alxc89@users.noreply.github.com>
2023-11-22 00:36:40 -0300

    ProcessandoArquivosGrandes

---
+++ youtube/c#/ProcessandoArquivosGrandes/ProcessandoArquivosGrandes/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProcessandoArquivosGrandes.ProcessandoArquivo;
+using System.Diagnostics;
+
+var before0 = GC.CollectionCount(0);
+var before1 = GC.CollectionCount(1);
+var before2 = GC.CollectionCount(2);
+
+var sw = new Stopwatch();
+sw.Start();
+//Run.Main(@"C:\Users\alexc\Documents\dev\ratings.csv");
+//Run.Main2(@"C:\Users\alexc\Documents\dev\ratings.csv");
+//Run.Main3(@"C:\Users\alexc\Documents\dev\ratings.csv");
+Run.Main4(@"C:\Users\alexc\Documents\dev\ratings.csv");
+sw.Stop();
+
+Console.WriteLine($"\nTime: {sw.ElapsedMilliseconds} ms");
+Console.WriteLine($"# Gen0: {GC.CollectionCount(0) - before0}");
+Console.WriteLine($"# Gen1: {GC.CollectionCount(1) - before1}");
+Console.WriteLine($"# Gen2: {GC.CollectionCount(2) - before2}");
+Console.WriteLine($"Memory: {Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} mb");
